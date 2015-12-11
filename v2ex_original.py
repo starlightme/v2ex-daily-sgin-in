91cb5ac0e698097c1a66c544ac7db95201071fa0
starlightme <jimmy66me@gmail.com>
2015-12-11 17:11:26 +0800

    transfer major document from chinese to english

--- v2ex_original.py
+++ v2ex_original.py
@@ -1,10 +1,11 @@
 #!/bin/env python
 # -*- coding : utf-8 -*-
 
+#导入模块
 import requests,re
 
-username = ''      
-password = ''    
+username = ''      #用户名
+password = ''    #密码
 login_url = 'http://v2ex.com/signin'
 home_url = 'http://www.v2ex.com'
 mission_url = 'http://www.v2ex.com/mission/daily'
@@ -24,12 +25,16 @@ def main():
   s = requests.session()
   d = {'u':username,'p':password,'next':'/'}
   page1 = s.get(login_url,headers=headers).text
+  #利用正则取出第一次的once值
   once1 = re.search(r'<input type="hidden" value="([0-9]+)" name="once" />',page1).group(1)
   d['once'] = once1
+  #模拟登陆
   s.post(login_url,data=d,headers=headers)
   page2 = s.get(mission_url,headers=headers).text
+  #取出第二次的once，这次进行字符串拼接成url后用get请求进行签到
   once2 = re.search(r'/mission/daily/redeem\?once=[0-9]+',page2).group(0)
   sign_url = home_url + once2
+  #签到
   page3 = s.get(sign_url,headers=headers).text
 
 
