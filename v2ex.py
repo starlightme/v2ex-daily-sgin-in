8b720ac3c91d60d1ad969d0960ad4f48b7d45363
starlightme <jimmy66me@gmail.com>
2017-04-14 14:14:41 +0800

    rewrite code and change to https

--- v2ex.py
+++ v2ex.py
@@ -1,13 +1,14 @@
 #!/bin/env python
 # -*- coding : utf-8 -*-
 
+
 import requests,re
 
 username = ''      
 password = ''    
-login_url = 'http://v2ex.com/signin'
-home_url = 'http://www.v2ex.com'
-mission_url = 'http://www.v2ex.com/mission/daily'
+login_url = 'https://v2ex.com/signin'
+home_url = 'https://www.v2ex.com'
+mission_url = 'https://www.v2ex.com/mission/daily'
 
 
 UA = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36"
@@ -22,16 +23,27 @@ headers = {
 
 def main():
   s = requests.session()
-  d = {'u':username,'p':password,'next':'/'}
+  d =  {'next':'/'}  
+
   page1 = s.get(login_url,headers=headers).text
   once1 = re.search(r'<input type="hidden" value="([0-9]+)" name="once" />',page1).group(1)
   d['once'] = once1
+  
+  name_code =  re.search(r'input type="text" class="sl" name="([0-9a-z]+)"',page1).group(1)
+  pwd_code  =  re.search(r'input type="password" class="sl" name="([0-9a-z]+)"',page1).group(1)
+  d[name_code] =  username
+  d[pwd_code]  =  password
   s.post(login_url,data=d,headers=headers)
+
   page2 = s.get(mission_url,headers=headers).text
   once2 = re.search(r'/mission/daily/redeem\?once=[0-9]+',page2).group(0)
   sign_url = home_url + once2
+  #sign
   page3 = s.get(sign_url,headers=headers).text
+  print 'finished!'
+
 
 
 if __name__ == '__main__':
   main()
+
